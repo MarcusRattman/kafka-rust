c69cbd1f51b2e93598ca9b99457e9cd87ef1589c
marcusrattman <zedrattman@gmail.com>
2025-06-03 13:47:14 +0700

    response

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 #![allow(unused_imports)]
 use std::{io::Write, net::TcpListener};
 
+mod models;
+mod tcp_helper;
+use models::Response;
+
+use tcp_helper::read_respond;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                stream.write_all(&[0, 0, 0, 0, 0, 0, 0, 7]).unwrap();
+                read_respond(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
